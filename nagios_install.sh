824adf9f6151dea52102dfb91b74bb4675a908a1
TheSkyEye <mailforgithubtheskyeye@yopmail.com>
2016-11-29 15:39:19 +0100

    Update nagios_install.sh

--- nagios_install.sh
+++ nagios_install.sh
@@ -1,2 +1,3 @@
 aptitude install -y make gcc curl && perl -MCPAN -e 'install Net::Address::IP::Local' && perl -MCPAN -e 'install DBI'
 curl -k "https://raw.githubusercontent.com/dorancemc/nagios_core/master/install_nagios.pl" >install_nagios.pl 
+chmod 755 install_nagios.pl && ./install_nagios.pl 
