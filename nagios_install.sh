97b3b98a7b8cbb0d761daea292f168472bd79289
TheSkyEye <mailforgithubtheskyeye@yopmail.com>
2016-11-29 15:18:27 +0100

    Create nagios_install.sh

---
+++ nagios_install.sh
@@ -0,0 +1,2 @@
+aptitude install -y make gcc curl && perl -MCPAN -e 'install Net::Address::IP::Local' && perl -MCPAN -e 'install DBI'
+curl -k "https://raw.githubusercontent.com/dorancemc/nagios_core/master/install_nagios.pl" >install_nagios.pl 
